867c4249c9cb7b86dad24a7b0466204cbd64959c
jsntn12 <jsantana@nperts-MacBook-Pro.local>
2023-05-04 09:05:53 -0400

    Updating unsaved file

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,37 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def addition():
+    a = request.args.get("a")
+    b = request.args.get("b")
+    result = add(int(a), int(b))
+
+    return str(result)
+
+@app.route('/sub')
+def subtract():
+    a = request.args.get("a")
+    b = request.args.get("b")
+    result = sub(int(a), int(b))
+
+    return str(result)
+
+@app.route('/mult')
+def multiply():
+    a = request.args.get("a")
+    b = request.args.get("b")
+    result = mult(int(a), int(b))
+
+    return str(result)
+
+@app.route('/div')
+def divide():
+    a = request.args.get("a")
+    b = request.args.get("b")
+    result = div(int(a), int(b))
+
+    return str(result)
